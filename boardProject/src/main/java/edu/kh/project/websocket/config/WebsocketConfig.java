64241cfd1635b49180cad48fc6448c3faf4dd38e
NTH <tang0929@naver.com>
2024-05-13 17:44:10 +0900

    20240513

--- boardProject/src/main/java/edu/kh/project/websocket/config/WebsocketConfig.java
+++ boardProject/src/main/java/edu/kh/project/websocket/config/WebsocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
+import edu.kh.project.websocket.handler.ChattingWebsocketHandler;
 import edu.kh.project.websocket.handler.NotificationWebsocketHandler;
 import edu.kh.project.websocket.handler.TestWebsocketHandler;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,10 @@ public class WebsocketConfig implements WebSocketConfigurer{
 	
 	// 알림 웹소켓 처리 객체 의존성 주입
 	private final NotificationWebsocketHandler notificationWebsocketHandler;
+	
+	
+	// 채팅과 관련된 웸소켓 처리 객체 의존성 주입
+	private final ChattingWebsocketHandler chattingWebsocketHandler;
 	
 	
 	// 웹소켓 핸들러를 등록하는 메서드
@@ -57,6 +62,19 @@ public class WebsocketConfig implements WebSocketConfigurer{
 							"http://127.0.0.1/", 
 							"http://192.168.10.14/")
 			.withSockJS();
+		
+		
+		
+		// 채팅처리 핸들러와 주소 연결
+				registry
+					.addHandler(chattingWebsocketHandler, "/chattingSock")
+					.addInterceptors(handshakeInterceptor)
+					.setAllowedOriginPatterns("http://localhost/", 
+									"http://127.0.0.1/", 
+									"http://192.168.10.14/")
+					.withSockJS();
+				
+				
 		
 		
 		
